13487695bb52e0410da7d9bea40ed850cb0edbc2
programmerr47 <m.spitsin@rambler-co.ru>
2017-01-02 11:06:58 +0400

    init commit

---
+++ app/src/main/java/com/github/programmerr47/navigationwidgets/AppActivity.java
@@ -0,0 +1,13 @@
+package com.github.programmerr47.navigationwidgets;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AppActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app);
+    }
+}
